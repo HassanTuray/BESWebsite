501334395724723ccfd6427a71462701fa4f241c
HFTuray <hassan_turay@Hotmail.com>
2025-10-14 01:29:11 -0400

    Added animated hero

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 
-
+'use client'
 // app/about/page.tsx
 import Image from "next/image";
 import OrgTree, { OrgTreeNode } from "../components/org-tree";
@@ -10,6 +10,15 @@ import { membershipZoneNode } from "./membershipZone";
 import { programsZoneNode } from "./programsZone";
 import * as page from "./aboutConstants";
 import Card from "../components/card";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText"
+
+import { useEffect, useRef} from 'react'
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SplitText);
 
 const rootLabel = (
   <div className="text-center">
@@ -25,42 +34,132 @@ const orgTreeData: OrgTreeNode = {
 }
 
 export default function AboutPage() {
+  useEffect ( () => {
+      (
+        async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default;
+          const ls = new LocomotiveScroll();
+  
+          const tl = gsap.timeline()
+  
+  
+        tl.fromTo(
+          '.fade-photo',
+          { opacity: 0, y: 40 },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1.2,
+            stagger: 0.25,
+            ease: 'power2.out',
+          }
+        )
+  
+        tl.fromTo(
+          '.hero-text',
+          { opacity: 0, y: 20 },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1.5,
+            ease: 'power2.out',
+          },
+          '-=0.5' 
+        )
+        }
+        
+      )()
+    },[]    
+    )
 
   return (
     <>
     <section className="px-25">
     {/* Huge page title with logos on both sides (one line, no white boxes) */}
-    <section className="mx-auto max-w-6xl px-4 py-12">
-      <div className="flex flex-nowrap items-center justify-center gap-3 sm:gap-6">
-        {/* Left logo (hidden on very small screens) */}
+    <section>
+      <div className="relative h-screen">
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <h1 className="whitespace-nowrap text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black leading-tight tracking-tight z-10" data-scroll data-scroll-speed="0.1">
+            <span className="hero-text split bg-gradient-to-r from-rose-600 via-[#FFD700] to-[#228B22] bg-clip-text text-transparent drop-shadow-[0_0_14px_rgba(255,215,0,0.22)]">
+              The Black Engineers Society
+            </span>
+          </h1>
+        </div>
+             
+        {/* Top right photo */}
         <Image
-          src="/slides/beslogo.png"   // ideally a transparent PNG/SVG
-          alt="BES logo"
-          width={96}
-          height={96}
-          className="hidden sm:block h-10 w-10 sm:h-14 sm:w-14 md:h-20 md:w-20 object-contain"
-          priority
+          id="image"
+          src="/slides/965A4015.JPG"
+          alt="collage image"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-10 left-215 z-0"
+          priority        
+          data-scroll
+          data-scroll-speed="0.2"
         />
 
-        <h1 className="whitespace-nowrap text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black leading-tight tracking-tight">
-          <span className="bg-gradient-to-r from-rose-600 via-[#FFD700] to-[#228B22] bg-clip-text text-transparent drop-shadow-[0_0_14px_rgba(255,215,0,0.22)]">
-            {page.c_titleText}
-          </span>
-        </h1>
-
-        {/* Right logo (hidden on very small screens) */}
+        {/* Top left photo */}
         <Image
-          src="/slides/nsbe-logo.png"  // ideally a transparent PNG/SVG
-          alt="BES crest"
-          width={96}
-          height={96}
-          className="hidden sm:block h-10 w-10 sm:h-14 sm:w-14 md:h-20 md:w-20 object-contain"
-          priority
-        />
+          src="/slides/BO3A5788.JPG"
+          alt="collage image"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-10 left-50 z-0"
+          priority   
+          data-scroll
+          data-scroll-speed="0.2"     
+        /> 
+
+        {/* Middle right photo */}
+        <Image
+          src="/slides/BO3A6506.JPG"
+          alt="collage image"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-100 left-250 z-0"
+          priority   
+          data-scroll
+          data-scroll-speed="0.3"     
+        /> 
+
+        {/* Bottom photo */}
+        <Image
+          src="/slides/BO3A8438.JPG"
+          alt="collage image"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-175 left-150 z-0"
+          priority  
+          data-scroll
+          data-scroll-speed="0.65"     
+        /> 
+
+        {/* Middle middle photo */}
+        <Image
+          src="/slides/BO3A8432.JPG"
+          alt="collage image"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-90 left-115 z-0"
+          priority    
+          data-scroll
+          data-scroll-speed="0.4"    
+        /> 
+
+        {/* Middle left photo */}
+        <Image
+          src="/slides/965A4052.JPG"
+          alt="collage image"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-80 left-0 z-0"
+          priority        
+          data-scroll
+          data-scroll-speed="0.5"
+        />       
       </div>
-
-      {/* Underline stays centered */}
-      <div className="mx-auto mt-4 h-[3px] w-48 rounded-full bg-gradient-to-r from-rose-600 via-[#FFD700] to-[#228B22]" />
+      <div className='h-[50vh]'></div>
     </section>
 
       {/* History Mission Statment Section */}
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,85 +1,140 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useRef} from 'react'
 import Image from "next/image";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText"
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SplitText);
 
 
 export default function ProgramsEventsPage() {
+  
   useEffect ( () => {
     (
       async () => {
         const LocomotiveScroll = (await import('locomotive-scroll')).default;
         const ls = new LocomotiveScroll();
+
+        const tl = gsap.timeline()
+
+
+      tl.fromTo(
+        '.fade-photo',
+        { opacity: 0, y: 40 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1.2,
+          stagger: 0.25,
+          ease: 'power2.out',
+        }
+      )
+
+      tl.fromTo(
+        '.hero-text',
+        { opacity: 0, y: 20 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1.5,
+          ease: 'power2.out',
+        },
+        '-=0.5' 
+      )
       }
+      
     )()
-  },[]
-    
-)
+  },[]    
+  )
 
   return (
+    
     <section>
       <div className="relative h-screen">
-        <div className="absolute inset-0 flex items-center justify-center border border-red-500 z-10">
-          <h1 className="whitespace-nowrap text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black leading-tight tracking-tight z-10">
-            <span className="bg-gradient-to-r from-rose-600 via-[#FFD700] to-[#228B22] bg-clip-text text-transparent drop-shadow-[0_0_14px_rgba(255,215,0,0.22)]">
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <h1 className="whitespace-nowrap text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black leading-tight tracking-tight z-10" data-scroll data-scroll-speed="0.1">
+            <span className="hero-text split bg-gradient-to-r from-rose-600 via-[#FFD700] to-[#228B22] bg-clip-text text-transparent drop-shadow-[0_0_14px_rgba(255,215,0,0.22)]">
               The Black Engineers Society
             </span>
           </h1>
         </div>
-
+       
+        {/* Top right photo */}
         <Image
+          id="image"
           src="/slides/965A4015.JPG"
           alt="collage image"
-          width={350}
-          height={350}
-          className="brightness-90 opacity-50 rounded-sm absolute top-10 left-200 z-0"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-10 left-215 z-0"
           priority        
+          data-scroll
+          data-scroll-speed="0.2"
         />
 
+        {/* Top left photo */}
         <Image
           src="/slides/BO3A5788.JPG"
           alt="collage image"
-          width={350}
-          height={350}
-          className="brightness-90 opacity-50 rounded-sm absolute top-10 left-20 z-0"
-          priority        
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-10 left-50 z-0"
+          priority   
+          data-scroll
+          data-scroll-speed="0.2"     
         /> 
 
+        {/* Middle right photo */}
         <Image
           src="/slides/BO3A6506.JPG"
           alt="collage image"
-          width={350}
-          height={350}
-          className="brightness-90 opacity-50 rounded-sm absolute top-100 left-250 z-0"
-          priority        
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-100 left-250 z-0"
+          priority   
+          data-scroll
+          data-scroll-speed="0.3"     
         /> 
 
+        {/* Bottom photo */}
         <Image
-          src="/slides/965A4015.JPG"
+          src="/slides/BO3A8438.JPG"
           alt="collage image"
-          width={350}
-          height={350}
-          className="brightness-90 opacity-50 rounded-sm absolute top-175 left-75 z-0"
-          priority        
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-175 left-150 z-0"
+          priority  
+          data-scroll
+          data-scroll-speed="0.65"     
         /> 
 
+        {/* Middle middle photo */}
         <Image
-          src="/slides/965A4015.JPG"
+          src="/slides/BO3A8432.JPG"
           alt="collage image"
-          width={350}
-          height={350}
-          className="brightness-90 opacity-50 rounded-sm absolute top-90 left-115 z-0"
-          priority        
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-90 left-115 z-0"
+          priority    
+          data-scroll
+          data-scroll-speed="0.4"    
         /> 
 
+        {/* Middle left photo */}
         <Image
-          src="/slides/965A4015.JPG"
+          src="/slides/965A4052.JPG"
           alt="collage image"
-          width={350}
-          height={350}
-          className="brightness-90 opacity-50 rounded-sm absolute top-80 left-0 z-0"
+          width={300}
+          height={300}
+          className="fade-photo brightness-90 opacity-50 rounded-sm absolute top-80 left-0 z-0"
           priority        
+          data-scroll
+          data-scroll-speed="0.5"
         />       
       </div>
+      <div className='h-[100vh]'></div>
     </section>
   );
 }
