bd21a1928dd46501726010c1df25a9be33ced721
HFTuray <hassan_turay@Hotmail.com>
2025-11-01 11:25:35 -0400

    Adding env variables

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -5,10 +5,12 @@ export async function POST(req) {
     const { name, email, role, message } = await req.json();
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
-        user: process.env.EMAIL_USER,  
-        pass: process.env.EMAIL_PASS,  
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
